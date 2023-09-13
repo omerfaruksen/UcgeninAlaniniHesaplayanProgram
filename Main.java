20f268e2d8e773b21540ba4058968dae477687c2
omerfaruksen <sen.faruk.93@gmail.com>
2023-09-13 14:43:41 +0300

    yorumlar

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-
+        //Değişkenlerimizi tanımlayalım
         int a,b,c;
         double alan, cevre, u;
+        //Kullanıcıdan verileri alalım
         Scanner input=new Scanner(System.in);
         System.out.print("Üçgenin a kenarını giriniz: ");
         a=input.nextInt();
@@ -11,6 +12,7 @@ public class Main {
         b=input.nextInt();
         System.out.print("Üçgenin c kenarını giriniz: ");
         c=input.nextInt();
+        //Hesaplamalarımız
         u=(a+b+c)/2;
         cevre=2*u;
         alan= Math.sqrt(u*(u-a)*(u-b)*(u-c));
